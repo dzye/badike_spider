34afb6dc88a810993fc9427c54c9a9949cf5c330
dzye <756611752@qq.com>
2017-02-24 17:57:15 +0800

    输出html添加样式

--- html_outputer.py
+++ html_outputer.py
@@ -11,14 +11,14 @@ class HtmlOutputer(object):
 
     def output_html(self):
         fout = open('output.html','w')
-        fout.write("<html><meta charset='utf-8'>")
+        fout.write("<html><meta charset='utf-8'><style>.c_url{width:15%;}.c_title{width:15%;}.c_summary{width:70%;}td{border:1px solid;text-align:center;}</style>")
         fout.write("<body>")
         fout.write("<table>")
         for data in self.datas:
             fout.write("<tr>")
-            fout.write("<td>%s</td>"%data['url'])
-            fout.write("<td>%s</td>" % data['title'].encode('utf8'))
-            fout.write("<td>%s</td>" % data['summary'].encode('utf8'))
+            fout.write("<td class=c_url>%s</td>"%data['url'])
+            fout.write("<td class=c_title>%s</td>" % data['title'].encode('utf8'))
+            fout.write("<td class=c_summary>%s</td>" % data['summary'].encode('utf8'))
             fout.write("</tr>")
         fout.write("</table>")
         fout.write("</body>")
